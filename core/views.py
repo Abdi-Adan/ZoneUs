cbe605ce23f18184338be316b1cdca8bc12dd994
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-05-12 06:28:14 +0300

    heroku prep

--- core/views.py
+++ core/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    context = {'n' : range(10) }
+    return render(request, 'index.html', context)
